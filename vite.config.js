80fd5d1367510da68fd1fe8c9a071d379614cb31
Minh Triet Khuong <87681993+triek@users.noreply.github.com>
2025-11-17 05:58:15 +0000

    add vue dev tools

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     vue(),
-    tailwindcss(), // 👈 this is the new bit
+    vueDevTools(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
